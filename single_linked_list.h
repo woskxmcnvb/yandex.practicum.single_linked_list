4af4af178525f768adfd69cc33b569dd970412a1
woskxm <woskxm@yandex.ru>
2022-09-04 13:50:50 +0300

    some work

---
+++ single_linked_list.h
@@ -0,0 +1,49 @@
+#pragma once
+
+template <typename Type>
+class SingleLinkedList {
+    // Узел списка
+    struct Node {
+        Node() = default;
+        Node(const Type& val, Node* next)
+            : value(val)
+            , next_node(next) {
+        }
+        Type value;
+        Node* next_node = nullptr;
+    };
+
+public:
+    SingleLinkedList() : size_(0) {}
+    
+    ~SingleLinkedList() {
+        Clear();
+    }
+
+    [[nodiscard]] size_t GetSize() const noexcept {
+        return size_;
+    }
+
+    [[nodiscard]] bool IsEmpty() const noexcept {
+        return size_ == 0;
+    }
+
+    void PushFront(const Type& value) {
+        head_.next_node = new Node(value, head_.next_node); 
+        ++size_; 
+    }
+
+    void Clear() noexcept {
+        while (head_.next_node) {
+            Node* tmp = head_.next_node;
+            head_.next_node = head_.next_node->next_node; 
+            delete tmp;      
+        }
+        size_ = 0;
+    }
+
+private:
+    // Фиктивный узел, используется для вставки "перед первым элементом"
+    Node head_;
+    size_t size_;
+};
